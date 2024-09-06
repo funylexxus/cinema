67f96090f070b94b0b9ecb8e07383a31266130d9
funylexxus <parens8school@gmail.com>
2024-09-06 21:17:07 +0300

    added form and style it

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $conn = mysqli_connect(hostname: $host,
                        username: $username,
                        password: $password,
                        database: $dbname);
-        
+    
 if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
 }           
@@ -30,7 +30,7 @@ $sql = "INSERT INTO message (name, body, priority, type)
 $stmt = mysqli_stmt_init($conn);
 
 if ( ! mysqli_stmt_prepare($stmt, $sql)) {
- 
+
     die(mysqli_error($conn));
 }
 
