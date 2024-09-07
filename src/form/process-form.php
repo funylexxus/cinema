0a1f5d16384757d6196395ee86d5aae8874760ba
funylexxus <parens8school@gmail.com>
2024-09-07 11:39:06 +0300

    created post request to table movies

--- src/form/process-form.php
+++ src/form/process-form.php
@@ -1,3 +1,42 @@
 <?php
 
+$title = $_POST["title"];
+$description = $_POST["description"];
+$release_date = $_POST["release_date"];
+$duration = filter_input(INPUT_POST, "duration", FILTER_SANITIZE_NUMBER_INT);
+$rating = filter_input(INPUT_POST, "rating", FILTER_SANITIZE_NUMBER_INT);
 
+
+$host = "localhost";
+$dbname = "cinema_db";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect(hostname: $host,
+username: $username,
+password: $password,
+database: $dbname);
+
+if (mysqli_connect_errno()) {
+die("Connection error: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO movies (title, description, release_date, duration, rating) VALUES (?, ?, ?, ?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "sssid",
+$title,
+$description,
+$release_date,
+$duration,
+$rating);
+
+mysqli_stmt_execute($stmt);
+
+echo "Chlen bol'she ne vstanet"; //????? WHAT IS THIS?
+?>
